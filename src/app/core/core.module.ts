6c06d5b465cb92057a7093d917f7e55a78c997b9
Houssem TRABELSI <hotrabelsi@vermeg.com>
2018-09-30 19:47:00 +0100

    import http client module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    LayoutModule
+    LayoutModule,
+    HttpClientModule
   ],
   declarations: [],
   exports: [
