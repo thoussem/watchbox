01868a476bd20de8d108211e378fd8ce677bf744
Houssem TRABELSI <hotrabelsi@vermeg.com>
2018-09-30 15:34:45 +0100

    add movie db url_api /key to envirement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  movieDbApiURL: 'https://api.themoviedb.org/3/',
+  movieDbApiKey: '9a3781c195c77aff5b3f1f51eadf0e12'
 };
 
 /*
